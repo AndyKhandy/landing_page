64bb1faeb76ea7f2dcd2afd29f1bc0a0fbe0b343
AndyKhandy <177374520+AndyKhandy@users.noreply.github.com>
2025-09-14 13:42:53 -0500

    Add sidebar functionality (no style yet)
    created a div that would act as the sidebar.
    Made a click evnet listener for the close icon and
    the menu icon to open and close the sidebar. Still
    need to style the sizebar for better visibility

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 const viewButton = document.querySelector("#changeMode");
+const openSide = document.querySelector("#menu");
+const closeSide = document.querySelector("#close");
+const sideBar = document.querySelector(".sidebar");
 
 
 viewButton.addEventListener("click",darkMode)
@@ -7,3 +10,11 @@ function darkMode()
 {
     document.body.classList.toggle("darkMode");
 }
+
+function changeSideBar()
+{
+    sideBar.classList.toggle("active");
+}
+
+openSide.addEventListener("click", changeSideBar)
+closeSide.addEventListener("click", changeSideBar);
